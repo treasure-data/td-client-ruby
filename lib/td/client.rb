405942565456a9f94d90583f695fee8a56e00a95
Jeffrey Yuan <jwyuan@alum.mit.edu>
2013-04-02 10:56:35 -0700

    fix hash get to for pig type in query

--- lib/td/client.rb
+++ lib/td/client.rb
@@ -122,7 +122,7 @@ class Client
   # => Job
   def query(db_name, q, result_url=nil, priority=nil, retry_limit=nil, opts={})
     # for compatibility, assume type is hive unless specifically specified
-    type = opts[:type] || :hive
+    type = opts[:type] || opts['type'] || :hive
     raise ArgumentError, "only 'hive' and 'pig' types are supported" unless [:hive, :pig].include? type
     job_id = @api.query(q, type, db_name, result_url, priority, retry_limit, opts)
     Job.new(self, job_id, type, q)
