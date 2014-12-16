03d8c95ec0cd933dbb4fd7b6faae379f83c26d67
Michele Caramello <michele@treasure-data.com>
2014-12-16 12:08:48 -0800

    suppress retrying message on POST request with no retrying
    CLT-573

--- lib/td/client/api.rb
+++ lib/td/client/api.rb
@@ -1377,7 +1377,11 @@ class API
           retry_delay *= 2
           retry
         else
-          $stderr.puts "Retrying stopped after #{@max_cumul_retry_delay} seconds."
+          if @retry_post_requests
+            $stderr.puts "Retrying stopped after #{@max_cumul_retry_delay} seconds."
+          else
+            $stderr.puts ""
+          end
           raise e
         end
       rescue => e
