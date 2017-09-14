dc45186750207a454a6fc2efc58052505279ee5c
NARUSE, Yui <naruse@airemix.jp>
2017-09-14 19:05:38 +0900

    Support focus feature of rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,6 +34,15 @@ require 'json'
 
 include TreasureData
 
+RSpec.configure do |config|
+  # This allows you to limit a spec run to individual examples or groups
+  # you care about by tagging them with `:focus` metadata. When nothing
+  # is tagged with `:focus`, all examples get run. RSpec also provides
+  # aliases for `it`, `describe`, and `context` that include `:focus`
+  # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
+  config.filter_run_when_matching :focus
+end
+
 shared_context 'common helper' do
   let :account_id do
     1
