cdd7400981c19b3c99a6ad4e88e30e8813559b91
huylenq <huy.lenq@gmail.com>
2021-01-13 14:40:42 +0700

    ci troubleshoot: additional logs

--- lib/td/client/api.rb
+++ lib/td/client/api.rb
@@ -524,7 +524,9 @@ private
       client.ssl_config.add_trust_ca(ssl_ca_file)
       client.ssl_config.verify_mode = OpenSSL::SSL::VERIFY_PEER
       # Disable SSLv3 connection in favor of POODLE Attack protection
+      puts ">>> new_client's ssl_config.options: #{client.ssl_config.options}"
       client.ssl_config.options |= OpenSSL::SSL::OP_NO_SSLv3
+      puts "<<< new_cient's ssl_config.options: #{client.ssl_config.options}"
     end
 
     header = {}
