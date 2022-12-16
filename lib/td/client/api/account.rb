2db0490e9e11e87e29d1556560561b5b99ba9254
son.do <son.do@treasure-data.com>
2022-12-16 09:25:09 +0700

    remove v3 core utilization api

--- lib/td/client/api/account.rb
+++ lib/td/client/api/account.rb
@@ -22,24 +22,5 @@ module Account
     return [account_id, plan, storage_size, guaranteed_cores, maximum_cores, created_at]
   end
 
-  # @param [Fixnum] from
-  # @param [Fixnum] to
-  # @return [Array]
-  def account_core_utilization(from, to)
-    params = { }
-    params['from'] = from.to_s if from
-    params['to'] = to.to_s if to
-    code, body, res = get("/v3/account/core_utilization", params)
-    if code != "200"
-      raise_error("Show account failed", res)
-    end
-    js = checked_json(body, %w[from to interval history])
-    from = Time.parse(js['from']).utc
-    to = Time.parse(js['to']).utc
-    interval = js['interval'].to_i
-    history = js['history']
-    return [from, to, interval, history]
-  end
-
 end
 end
